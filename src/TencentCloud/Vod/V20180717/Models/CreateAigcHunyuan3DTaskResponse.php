8edb68cdb21ea75d0de22020369ea11212cc8c32
tencentcloudapi <tencentcloudapi@tencent.com>
2026-09-18 04:03:37 +0800

    release 3.0.1670

--- src/TencentCloud/Vod/V20180717/Models/CreateAigcHunyuan3DTaskResponse.php
+++ src/TencentCloud/Vod/V20180717/Models/CreateAigcHunyuan3DTaskResponse.php
@@ -18,26 +18,19 @@ namespace TencentCloud\Vod\V20180717\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * DescribePersonSamples返回参数结构体
+ * CreateAigcHunyuan3DTask返回参数结构体
  *
- * @method integer getTotalCount() 获取<p>符合条件的记录总数。</p>
- * @method void setTotalCount(integer $TotalCount) 设置<p>符合条件的记录总数。</p>
- * @method array getPersonSet() 获取<p>人物信息。</p>
- * @method void setPersonSet(array $PersonSet) 设置<p>人物信息。</p>
+ * @method string getTaskId() 获取<p>任务 ID。</p>
+ * @method void setTaskId(string $TaskId) 设置<p>任务 ID。</p>
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class DescribePersonSamplesResponse extends AbstractModel
+class CreateAigcHunyuan3DTaskResponse extends AbstractModel
 {
     /**
-     * @var integer <p>符合条件的记录总数。</p>
+     * @var string <p>任务 ID。</p>
      */
-    public $TotalCount;
-
-    /**
-     * @var array <p>人物信息。</p>
-     */
-    public $PersonSet;
+    public $TaskId;
 
     /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
@@ -45,8 +38,7 @@ class DescribePersonSamplesResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param integer $TotalCount <p>符合条件的记录总数。</p>
-     * @param array $PersonSet <p>人物信息。</p>
+     * @param string $TaskId <p>任务 ID。</p>
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -62,17 +54,8 @@ class DescribePersonSamplesResponse extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("TotalCount",$param) and $param["TotalCount"] !== null) {
-            $this->TotalCount = $param["TotalCount"];
-        }
-
-        if (array_key_exists("PersonSet",$param) and $param["PersonSet"] !== null) {
-            $this->PersonSet = [];
-            foreach ($param["PersonSet"] as $key => $value){
-                $obj = new AiSamplePerson();
-                $obj->deserialize($value);
-                array_push($this->PersonSet, $obj);
-            }
+        if (array_key_exists("TaskId",$param) and $param["TaskId"] !== null) {
+            $this->TaskId = $param["TaskId"];
         }
 
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
